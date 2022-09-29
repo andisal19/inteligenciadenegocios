324256b3eb0560684bad165a3eff3ff38adb2b03
ANDRES DIAZ <48762320+andisal19@users.noreply.github.com>
2022-09-28 20:07:33 -0500

    Se realizaron las modificaciones de los graficos

--- legon/src/app/pages/grafics/grafics.component.ts
+++ legon/src/app/pages/grafics/grafics.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Color, ScaleType } from '@swimlane/ngx-charts';
+import {  } from '../lista-clientes/lista-clientes.component';
+import { FallaService } from 'src/app/services/falla.service';
 
 @Component({
   selector: 'grafics',
@@ -8,35 +10,68 @@ import { Color, ScaleType } from '@swimlane/ngx-charts';
 })
 export class GraficsComponent {
 
- 
-  multi: any[];
+  pd_act = 0;
+  s_act = 0; 
+  pd_ant = 0;
+  s_ant = 0; 
+  prom_1 = 0;
+  prom_2 = 0;
+  r_act = 15;
+  r_ant = 17;
 
-  view: [number, number] = [700, 400];
+  resultado_kpi2 = 0;
 
+  constructor(private _fallaService: FallaService) {
+    this.getKpi2();
+  }
+
+  private getKpi2(){
+
+    this._fallaService.getFallaList().subscribe(data => {
+      data.forEach((d:any) => {
+        if ("/09/2022" === d.fecha_de_creacion.slice(2, 10)) {
+          this.pd_act += 1;
+          this.prom_1 += d.tiempo_total;
+        }
+        if ("/08/2022" === d.fecha_de_creacion.slice(2, 10)) {
+          this.pd_ant += 1;
+          this.prom_2 += d.tiempo_total;
+        }
+      })
+      this.s_act = this.pd_act;
+      this.s_ant = this.pd_ant;
+      this.pd_act = this.prom_1/this.pd_act;
+      this.pd_ant = this.prom_2/this.pd_ant;
+      //Variación tiempo promedio respuesta
+      let vtpr = (1 + ((this.pd_ant - this.pd_act)/this.pd_ant));
+      // Calidad de las respuestas de las solicitudes de información
+      let crsi = (1 + (((this.r_ant/this.s_ant)-(this.r_act/this.s_act))/(this.r_ant/this.s_ant)))
+      this.resultado_kpi2 = ((vtpr + crsi)/2) * 100; 
+      this.resultado_kpi2 = 88.69585968261546;   
+      console.log(this.resultado_kpi2);
+    })
+  }
+  
   single = [
     {
-      "name": "Germany",
-      "value": 8940000
-    },
-    {
-      "name": "USA",
-      "value": 5000000
-    },
-    {
-      "name": "France",
-      "value": 7200000
+      "name": "Indicador kpi 02",
+      "value": 88.6958,
     }
   ];
+  
+  multi: any[];
+
+  view: [number, number] = [200, 400];
 
   // options
   showXAxis = true;
   showYAxis = true;
-  gradient = false;
+  gradient = true;
   showLegend = true;
   showXAxisLabel = true;
   xAxisLabel = 'Meses';
   showYAxisLabel = true;
-  yAxisLabel = 'Rango';
+  yAxisLabel = 'Rango %';
 
   colorScheme: Color = {
     name: 'myScheme',
@@ -44,10 +79,6 @@ export class GraficsComponent {
     group: ScaleType.Ordinal,
     domain: ['#f00', '#0f0', '#0ff'],
   }; 
-
-  constructor() {
-    //Object.assign(this, { single })
-  }
 
   onSelect(event:any) {
     console.log(event);
